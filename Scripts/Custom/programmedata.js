1f8f0a1f0ae0c82952f282fa046af9387b683fc9
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-05-18 00:47:23 +0200

    Added names to heats for friday

--- Scripts/Custom/programmedata.js
+++ Scripts/Custom/programmedata.js
@@ -1,4 +1,4 @@
-var programme = {
+﻿var programme = {
 	friday: {
 		header: "Friday",
 		subtitle: "Competition Heats",
@@ -8,39 +8,373 @@ var programme = {
 						starttime: new Date(2013, 5, 17, 9, 0, 0, 0),
 						heattimelimit: 20,
 						heatcount: 3,
-						spartanHeat: 2
+						heats: [
+							{
+								names: [
+									"Reebok CrossFit KATLA",
+									"Crossfit Nottingham",
+									"CrossFit Basel 2",
+									"Reebok Crossfit Louvre French Invictus Team",
+									"Team CFH Iceland",
+									"Tyr",
+									"Team Hamar",
+									"CrossFit Basel",
+									"CrossFit M",
+									"Team Boat Shed"
+								]
+							},
+							{
+								names: [
+									"CrossFit Turicum",
+									"Team Spartan Mentality",
+									"CrossFit Copenhagen 1",
+									"CrossFit Oslo",
+									"CrossFit Sport",
+									"CrossFit Falun Måndagsklubben",
+									"CrossFit Leeds",
+									"Crossfit Aalborg",
+									"Crossfit Faction",
+									"CrossFit Black Five"
+								]
+							},
+							{
+								names: [
+									"Team Butchers Lab",
+									"CrossFit Solid",
+									"CrossFit Pori",
+									"Team Reebok CrossFit Nordic",
+									"CrossFit Reykjavik",
+									"Thor",
+									"CrossFit Malmö The Other guys",
+									"Team Butcher's Garage",
+									"CrossFit Bergen",
+									"CrossFit Bath"
+								]
+							}
+						]
 					},
 					{
 						name: "Womens workout 1",
 						starttime: new Date(2013, 5, 17, 10, 15, 0, 0),
 						heattimelimit: 15,
 						heatcount: 4,
-						spartanHeat: 1
+						heats: [
+							{
+								names: [
+									"Labrunie Elise",
+									"Hildur Grétarsdóttir",
+									"Carmen Vicente Cabañas",
+									"Sara Steinmann",
+									"Hrund Scheving",
+									"Eva Cortiella",
+									"Gina Yates",
+									"Sveina Björk Karlsdóttir",
+									"Llara Romanowski"
+								]
+							},
+							{
+								names: [
+									"Gwendolin Sona",
+									"Friða Ammendrup",
+									"Carmen Bosmans",
+									"Amy Laffan",
+									"Sanne Rooijmans",
+									"louise tjäder",
+									"Saara Laaksonen",
+									"Anna Fernette",
+									"Sónia Alves",
+									"Kelly Friel"
+								]
+							},
+							{
+								names: [
+									"Nuša Repovž",
+									"Nadine Steinger",
+									"Marianna Tzourtzek",
+									"Ragnheiður Sara Sigmundsdottir",
+									"Camilla Salomonsson Hellman",
+									"Steph Dekker",
+									"Essi Koskinen",
+									"Rebecca Pykett",
+									"Brie Pineiro",
+									"marina Novelli"
+								]
+							},
+							{
+								names: [
+									"Caroline Fryklund",
+									"Kristin Holte",
+									"Oxana Slivenko",
+									"Emma Rosengren",
+									"Thuridur Erla Helgadottir",
+									"Samantha Briggs",
+									"Martina Barbaro",
+									"Katrin Tanja Davidsdottir",
+									"Brenda Koolhaas",
+									"Patricia Strenius",
+									"Nicola Simpson",
+									"Hayley Knowles"
+								]
+							}
+						],
 					},
 					{
 						name: "Mens workout 1",
 						starttime: new Date(2013, 5, 17, 11, 30, 0, 0),
 						heattimelimit: 15,
-						heatcount: 4
+						heatcount: 4,
+						heats: [
+							{
+								names: [
+									"Joe Baylock",
+									"Mattia Balella Guerra",
+									"Martin Wodler",
+									"Phil Hesketh",
+									"Richard Vanmeerbeek",
+									"Bodnár Ádám",
+									"viktor larsson",
+									"John Reid",
+									"Marcus Herou",
+									"stefano italiano",
+									"Joakim Törnvi",
+									"Jonna Koski"
+								]
+							},
+							{
+								names: [
+									"Lee Howa",
+									"Marc Ordeig",
+									"Jindřich Tovaryš",
+									"Philipp Warkowski",
+									"Gigord Yohann",
+									"Karl Thorpe",
+									"Robert Manlove",
+									"Jo Neusser",
+									"Leigh Bevan",
+									"Rasmus Wisbech Andersen",
+									"Tom Schwander",
+									"Árni Freyr Bjarnason"
+								]
+							},
+							{
+								names: [
+									"TImmy Thernström",
+									"David Santa Cruz Borgstrand",
+									"Dafydd Dennis",
+									"Christer Idland",
+									"Kristoffer Klausen",
+									"Jonas Müller",
+									"Alex Clarke",
+									"Mareck Bigler",
+									"Greg Schregle",
+									"Riccardo Donati",
+									"Rob Martin",
+									"Carlos Albaladejo"
+								]
+							},
+							{
+								names: [
+									"Stefano Migliorini",
+									"Stuart Trees",
+									"Björgvin Karl Guðmundsson",
+									"Mikko Aronpää",
+									"Lacee Kovacs",
+									"Mikko salo",
+									"Lukas Högberg",
+									"Frederik Aegidius",
+									"Jordan Wallace",
+									"Davide Marini",
+									"Steven Fawcett",
+									"Jakob Magnússon"
+								]
+							}
+						],
 					},
 					{
 						name: "Teams workout 2/3",
 						starttime: new Date(2013, 5, 17, 13, 0, 0, 0),
 						heattimelimit: 25,
 						heatcount: 3,
-						spartanHeat: 2
+						heats: [
+							{
+								names: [
+									"Reebok CrossFit KATLA",
+									"Crossfit Nottingham",
+									"CrossFit Basel 2",
+									"Reebok Crossfit Louvre French Invictus Team",
+									"Team CFH Iceland",
+									"Tyr",
+									"Team Hamar",
+									"CrossFit Basel",
+									"CrossFit M",
+									"Team Boat Shed"
+								]
+							},
+							{
+								names: [
+									"CrossFit Turicum",
+									"Team Spartan Mentality",
+									"CrossFit Copenhagen 1",
+									"CrossFit Oslo",
+									"CrossFit Sport",
+									"CrossFit Falun Måndagsklubben",
+									"CrossFit Leeds",
+									"Crossfit Aalborg",
+									"Crossfit Faction",
+									"CrossFit Black Five"
+								]
+							},
+							{
+								names: [
+									"Team Butchers Lab",
+									"CrossFit Solid",
+									"CrossFit Pori",
+									"Team Reebok CrossFit Nordic",
+									"CrossFit Reykjavik",
+									"Thor",
+									"CrossFit Malmö The Other guys",
+									"Team Butcher's Garage",
+									"CrossFit Bergen",
+									"CrossFit Bath"
+								]
+							}
+						],
 					},
 					{
 						name: "Womens workout 2/3",
 						starttime: new Date(2013, 5, 17, 14, 30, 0, 0),
 						heattimelimit: 20,
 						heatcount: 4,
-						spartanHeat: 1
+						heats: [
+							{
+								names: [
+									"Labrunie Elise",
+									"Hildur Grétarsdóttir",
+									"Carmen Vicente Cabañas",
+									"Sara Steinmann",
+									"Hrund Scheving",
+									"Eva Cortiella",
+									"Gina Yates",
+									"Sveina Björk Karlsdóttir",
+									"Llara Romanowski"
+								]
+							},
+							{
+								names: [
+									"Gwendolin Sona",
+									"Friða Ammendrup",
+									"Carmen Bosmans",
+									"Amy Laffan",
+									"Sanne Rooijmans",
+									"louise tjäder",
+									"Saara Laaksonen",
+									"Anna Fernette",
+									"Sónia Alves",
+									"Kelly Friel"
+								]
+							},
+							{
+								names: [
+									"Nuša Repovž",
+									"Nadine Steinger",
+									"Marianna Tzourtzek",
+									"Ragnheiður Sara Sigmundsdottir",
+									"Camilla Salomonsson Hellman",
+									"Steph Dekker",
+									"Essi Koskinen",
+									"Rebecca Pykett",
+									"Brie Pineiro",
+									"marina Novelli"
+								]
+							},
+							{
+								names: [
+									"Caroline Fryklund",
+									"Kristin Holte",
+									"Oxana Slivenko",
+									"Emma Rosengren",
+									"Thuridur Erla Helgadottir",
+									"Samantha Briggs",
+									"Martina Barbaro",
+									"Katrin Tanja Davidsdottir",
+									"Brenda Koolhaas",
+									"Patricia Strenius",
+									"Nicola Simpson",
+									"Hayley Knowles"
+								]
+							}
+						],
 					},
 					{
 						name: "Mens workout 2/3",
 						starttime: new Date(2013, 5, 17, 16, 10, 0, 0),
 						heattimelimit: 20,
+						heats: [
+							{
+								names: [
+									"Joe Baylock",
+									"Mattia Balella Guerra",
+									"Martin Wodler",
+									"Phil Hesketh",
+									"Richard Vanmeerbeek",
+									"Bodnár Ádám",
+									"viktor larsson",
+									"John Reid",
+									"Marcus Herou",
+									"stefano italiano",
+									"Joakim Törnvi",
+									"Jonna Koski"
+								]
+							},
+							{
+								names: [
+									"Lee Howa",
+									"Marc Ordeig",
+									"Jindřich Tovaryš",
+									"Philipp Warkowski",
+									"Gigord Yohann",
+									"Karl Thorpe",
+									"Robert Manlove",
+									"Jo Neusser",
+									"Leigh Bevan",
+									"Rasmus Wisbech Andersen",
+									"Tom Schwander",
+									"Árni Freyr Bjarnason"
+								]
+							},
+							{
+								names: [
+									"TImmy Thernström",
+									"David Santa Cruz Borgstrand",
+									"Dafydd Dennis",
+									"Christer Idland",
+									"Kristoffer Klausen",
+									"Jonas Müller",
+									"Alex Clarke",
+									"Mareck Bigler",
+									"Greg Schregle",
+									"Riccardo Donati",
+									"Rob Martin",
+									"Carlos Albaladejo"
+								]
+							},
+							{
+								names: [
+									"Stefano Migliorini",
+									"Stuart Trees",
+									"Björgvin Karl Guðmundsson",
+									"Mikko Aronpää",
+									"Lacee Kovacs",
+									"Mikko salo",
+									"Lukas Högberg",
+									"Frederik Aegidius",
+									"Jordan Wallace",
+									"Davide Marini",
+									"Steven Fawcett",
+									"Jakob Magnússon"
+								]
+							}
+						],
 						heatcount: 4
 					}
 		]
@@ -54,6 +388,7 @@ var programme = {
 						starttime: new Date(2013, 5, 18, 8, 30, 0, 0),
 						heattimelimit: 30,
 						heatcount: 3,
+						heats: [],
 						spartanHeat: 2
 					},
 					{
@@ -61,12 +396,14 @@ var programme = {
 						starttime: new Date(2013, 5, 18, 10, 0, 0, 0),
 						heattimelimit: 30,
 						heatcount: 4,
+						heats: [],
 						spartanHeat: 1
 					},
 					{
 						name: "Mens workout 4",
 						starttime: new Date(2013, 5, 18, 12, 45, 0, 0),
 						heattimelimit: 30,
+						heats: [],
 						heatcount: 4
 					},
 					{
@@ -74,6 +411,7 @@ var programme = {
 						starttime: new Date(2013, 5, 18, 15, 0, 0, 0),
 						heattimelimit: 15,
 						heatcount: 3,
+						heats: [],
 						spartanHeat: 2
 					},
 					{
@@ -81,12 +419,14 @@ var programme = {
 						starttime: new Date(2013, 5, 18, 16, 0, 0, 0),
 						heattimelimit: 12,
 						heatcount: 4,
+						heats: [],
 						spartanHeat: 1
 					},
 					{
 						name: "Mens workout 5",
 						starttime: new Date(2013, 5, 18, 17, 0, 0, 0),
 						heattimelimit: 12,
+						heats: [],
 						heatcount: 4
 					}
 		]
@@ -100,6 +440,7 @@ var programme = {
 						starttime: new Date(2013, 5, 19, 11, 50, 0, 0),
 						heattimelimit: 20,
 						heatcount: 3,
+						heats: [],
 						spartanHeat: 2
 					},
 					{
@@ -107,12 +448,14 @@ var programme = {
 						starttime: new Date(2013, 5, 19, 13, 0, 0, 0),
 						heattimelimit: 20,
 						heatcount: 4,
+						heats: [],
 						spartanHeat: 1
 					},
 					{
 						name: "Mens workout 6",
 						starttime: new Date(2013, 5, 19, 14, 30, 0, 0),
 						heattimelimit: 20,
+						heats: [],
 						heatcount: 4
 					},
 					{
@@ -120,6 +463,7 @@ var programme = {
 						starttime: new Date(2013, 5, 19, 16, 20, 0, 0),
 						heattimelimit: 25,
 						heatcount: 3,
+						heats: [],
 						spartanHeat: 2
 					},
 					{
